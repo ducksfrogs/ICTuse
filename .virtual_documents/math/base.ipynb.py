ea8b215a484a6cd4cd334deaa37ccc949de2b231
ma <ma_yamaki@yahoo.com>
2022-10-10 13:39:29 +0900

    math

---
+++ .virtual_documents/math/base.ipynb.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+a = np.arange(15).reshape(3,5)
+
+
+a
+
+
+a.shape
+
+
+a.dtype.name
+
+
+b = np.linspace(0,np.pi,3)
+
+
+b
+
+
+a = np.arange(12)**2
+
+
+a
+
+
+a = np.arange(12).reshape(3,4)
+
+
+i = np.array([[0,1],
+             [1,2]])
+
+j = np.array([[2,1],
+             [3,3]])
+
+
+a[i,j]
+
+
+i
+
+
+a[2]
+
+
+a
+
+
+a[0,:]
+
+
+a >4
+
+
+
---
+++ .virtual_documents/math/sympyBase.ipynb.py
@@ -0,0 +1,31 @@
+import sympy
+
+
+sympy.sqrt(8)
+
+
+x, y = sympy.symbols('x y')
+
+
+expr = x +2*y
+
+
+expr+1
+
+
+expr
+
+
+x*expr
+
+
+expanded_expr = x*expr
+
+
+expanded_expr
+
+
+sympy.expand(expanded_expr)
+
+
+
