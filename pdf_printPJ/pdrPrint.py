18ca2f99528e896dbbcb204dcd036b78f4a78084
ducksfrogs <ma_yamaki@yahoo.com>
2023-06-06 06:28:50 +0900

    gridmom

--- pdf_printPJ/pdrPrint.py
+++ pdf_printPJ/pdrPrint.py
@@ -1,5 +1,5 @@
 import glob
-import PyPDF2 as pd
+import PyPDF2 as pdf
 
 
 lst = []
@@ -9,8 +9,17 @@ for i in range(0, n):
     ele = int(input("Enter you want to print"))
     lst.append(ele)
 
-writer = pd.Pdf.FileWriter()
+writer = pdf.PdfFileWriter()
 files = glob.glob('../data/*.pdf')
 
 for i in range(len(files)):
+    reader = pdf.PdfFileReader(files[i])
+
+    for l in range(0, reader.numPages):
+        if l in lst:
+            page = reader.pages[l]
+            writer.addPage(page)
+        
+with open("comp.pdf", 'wb') as f:
+    writer.write(f)
     
